6ad2667d4d068a2f387bbe1c826b0814f97b859b
pipeh <felipehm.eng@gmail.com>
2025-06-08 21:33:05 -0400

    feat: First DAG working till Gradio stage

--- lab9/dags/dag_lineal.py
+++ lab9/dags/dag_lineal.py
@@ -1,7 +1,7 @@
 from datetime import datetime, date
 
 from airflow import DAG
-from airflow.operators.python import EmptyOperator
+from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 
@@ -12,7 +12,6 @@ with DAG(
     description="A lineal dag",
     start_date = datetime(2024,10,1),
     catchup=False,
-    default_args = {"date": str(date.today())},
     schedule=None
 ) as dag:
     
@@ -25,7 +24,7 @@ with DAG(
 
     download_dataset_task = BashOperator(
         task_id='Download_dataset',
-        bash_command='curl -o ./{{ dag.default_args.date }}/raw/data_1.csv https://gitlab.com/eduardomoyab/laboratorio-13/-/raw/main/files/data_1.csv'
+        bash_command='curl -o $AIRFLOW_HOME/{{ ds }}/raw/data_1.csv https://gitlab.com/eduardomoyab/laboratorio-13/-/raw/main/files/data_1.csv'
     )
 
     holdout_task = PythonOperator(
--- lab9/dags/hiring_functions.py
+++ lab9/dags/hiring_functions.py
@@ -1,4 +1,5 @@
 import os
+import json
 import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -16,17 +17,20 @@ set_config(transform_output="pandas")
 # misma semilla para slpit y para entrenamiento
 seed = 123
 
+# Variable home
+home_dir = os.getenv('AIRFLOW_HOME')
+
 def create_folders(**kwargs):
-    date = kwargs.get("date")
+    date = kwargs.get("ds")
     os.mkdir(date)
-    os.mkdir(f"{date}/raw")
-    os.mkdir(f"{date}/splits")
-    os.mkdir(f"{date}/models")
+    os.mkdir(f"{home_dir}/{date}/raw")
+    os.mkdir(f"{home_dir}/{date}/splits")
+    os.mkdir(f"{home_dir}/{date}/models")
     return
 
 def split_data(**kwargs):
-    dir = f"{kwargs.get('date')}"
-    df = pd.read_csv(f"{dir}/raw/data_1.csv")
+    dir = f"{kwargs.get('ds')}"
+    df = pd.read_csv(f"{home_dir}/{dir}/raw/data_1.csv")
     target_col = "HiringDecision"
     X = df.drop(columns=[target_col])
     X_cols = X.columns
@@ -40,24 +44,23 @@ def split_data(**kwargs):
     test_df = pd.DataFrame(X_test, columns=X_cols)
     test_df[target_col] = y_test
 
-    train_df.to_csv(f"{dir}/splits/train.csv")
-    test_df.to_csv(f"{dir}/test.csv")
+    train_df.to_csv(f"{home_dir}/{dir}/splits/train.csv", index=False)
+    test_df.to_csv(f"{home_dir}/{dir}/splits/test.csv", index=False)
 
 class TypeTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, type):
-        self.type_to_transform=type
-        return self
+    def __init__(self, type_to_transform):
+        self.type_to_transform = type_to_transform
     
-    def fit(self, X, y =None):
+    def fit(self, X, y=None):
         return self
     
     def transform(self, X):
         return X.astype(self.type_to_transform)
 
 def preprocess_and_train(**kwargs):
-    dir = f"{kwargs.get('date')}"
-    train_df = pd.read_csv(f"{dir}/splits/train.csv")
-    test_df = pd.read_csv(f"{dir}/splits/test.csv")
+    dir = f"{kwargs.get('ds')}"
+    train_df = pd.read_csv(f"{home_dir}/{dir}/splits/train.csv")
+    test_df = pd.read_csv(f"{home_dir}/{dir}/splits/test.csv")
 
     # basandose en data_1_report, se puede concluir que las unicas variables que necesitan one_hot_encoding son EducationLevel RecruitmentStrategy.
     # el resto de columnas "categoricas" solo necesita un cambio de tipos.
@@ -65,22 +68,24 @@ def preprocess_and_train(**kwargs):
 
     encode_cols = ["EducationLevel", "RecruitmentStrategy"]
     astype_cols = ["PreviousCompanies"]
-    minmax_cols = ["Age","ExperienceYears","PreviousCompanies", "DistanceFromCompany", "InterviewScore", "SkillScore"]
+    minmax_cols = ["Age", "ExperienceYears", "PreviousCompanies", "DistanceFromCompany", "InterviewScore", "SkillScore"]
 
-    encoder = OneHotEncoder()
+    encoder = OneHotEncoder(sparse_output=False)
     scaler = MinMaxScaler(feature_range=(0,1))
-    type_transformer = TypeTransformer(int)
+    type_transformer = TypeTransformer(type_to_transform=int)
 
     encoding_transformer = ColumnTransformer([
         ("One Hot Encoding", encoder, encode_cols),
         ("Type transformer", type_transformer, astype_cols)
     ],
-    remainder="passthrough")
+    remainder="passthrough",
+    verbose_feature_names_out=False)
 
     scaler_transformer = ColumnTransformer([
         ("MinMax Scaler", scaler, minmax_cols)
     ], 
-    remainder = "passthrough")
+    remainder="passthrough",
+    verbose_feature_names_out=False)
     
     rf = RandomForestClassifier()
 
@@ -92,21 +97,21 @@ def preprocess_and_train(**kwargs):
 
     X = train_df.drop(columns=["HiringDecision"])
     y = train_df["HiringDecision"]
-    pipeline.fit(X,y)
+    pipeline.fit(X, y)
 
-    joblib.dump(pipeline, f"{dir}/models/pipeline.joblib")
+    joblib.dump(pipeline, f"{home_dir}/{dir}/models/pipeline.joblib")
     X_test = test_df.drop(columns=["HiringDecision"])
     y_test = test_df["HiringDecision"]
-    predictions = pipeline.transform(X_test)
+    predictions = pipeline.predict(X_test)
 
-    report = classification_report(y_true=y_test, y_pred=predictions)
-    accuracy = report["1"]["accuracy"]
+    report = classification_report(y_true=y_test, y_pred=predictions,output_dict=True)
+    accuracy = report["accuracy"]
     f1_score = report["1"]["f1-score"]
 
     print(f"Accuracy for positive class: {accuracy:.2f}")
     print(f"F1-Score for positive class: {f1_score:.2f}")
 
-def predict(file,model_path):
+def predict(file, model_path):
 
     pipeline = joblib.load(model_path)
     input_data = pd.read_json(file)
@@ -118,8 +123,8 @@ def predict(file,model_path):
 
 
 def gradio_interface(**kwargs):
-    dir = f"{kwargs.get('date')}"
-    model_path= f"{dir}/models/pipeline.joblib" #Completar con la ruta del modelo entrenado
+    dir = f"{kwargs.get('ds')}"
+    model_path= f"{home_dir}/{dir}/models/pipeline.joblib" # Completar con la ruta del modelo entrenado
 
     interface = gr.Interface(
         fn=lambda file: predict(file, model_path),
